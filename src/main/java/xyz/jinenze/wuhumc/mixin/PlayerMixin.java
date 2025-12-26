562882b13ade097beab7ddd254fb28bbc5cd205f
Jinenze <jinenze0810@gmail.com>
2025-12-26 13:33:32 +0800

    feat: make player always can eat food

---
+++ src/main/java/xyz/jinenze/wuhumc/mixin/PlayerMixin.java
@@ -0,0 +1,16 @@
+package xyz.jinenze.wuhumc.mixin;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Player.class)
+public final class PlayerMixin {
+    @Inject(method = "canEat", at = @At("HEAD"), cancellable = true)
+    private void onCanEat(boolean bl, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+}

fd2f917f68ee36a6961327b150c2e70f24a44dd4
Jinenze <jinenze0810@gmail.com>
2025-12-30 16:22:51 +0800

    refactor: replace deprecated config screen api usage

--- src/client/java/xyz/jinenze/wuhumc/client/config/ModMenuEntry.java
+++ src/client/java/xyz/jinenze/wuhumc/client/config/ModMenuEntry.java
@@ -2,11 +2,11 @@ package xyz.jinenze.wuhumc.client.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class ModMenuEntry implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ClientConfigWrapper.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(ClientConfigWrapper.class, parent).get();
     }
 }
